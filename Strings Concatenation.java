c4f3e48d01a2455d510d7da8786eb63cb676b764
Sheoran01 <81296541+Sheoran01@users.noreply.github.com>
2022-05-29 16:49:32 +0530

    Create Strings Concatenation.java

---
+++ Strings Concatenation.java
@@ -0,0 +1,13 @@
+mport java.util.Scanner;
+
+public class P7 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        String n1 = sc.nextLine();
+
+        String n2 = n + n1;
+        System.out.println(n2);
+
+    }
+}
